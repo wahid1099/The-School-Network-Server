9fa8834b90c8d917292e28b9dbe0676825243b2b
KaziMdMehediHasan <=>
2022-02-24 15:31:59 +0600

    request care route added

---
+++ models/Student/requestCare.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const requestSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: "string",
+      required: true,
+    },
+    middleName: {
+      type: "string",
+      required: true,
+    },
+    lastName: {
+      type: "string",
+      required: true,
+    },
+    class: {
+      type: "number",
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    email: {
+      type: "string",
+      required: true,
+    },
+    courseName: {
+      type: "string",
+      required: true,
+    },
+    teacherName: {
+      type: "string",
+      required: true,
+    },
+    section: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("RequestCare", requestSchema);
--- routes/Student/student.js
+++ routes/Student/student.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Student = require("../../models/Student/studentModels");
+const RequestCare = require("../../models/Student/requestCare");
+const { db } = require("../../models/Student/studentModels");
 
 //Student notes Submit
 router.post("/notesSubmit", async (req, res) => {
@@ -12,4 +14,28 @@ router.post("/notesSubmit", async (req, res) => {
   }
 });
 
+//student request care
+
+router.post("/requestCare", async (req, res) => {
+  // model creation through the schema
+  const newRequest = new RequestCare(req.body);
+  try {
+    const savedRequest = await newRequest.save();
+    res.status(200).json(savedRequest);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get the data from the database
+
+router.get("/requestCare", async (req, res) => {
+  try {
+    // no need for database name, only the schema name is enough to fetch data
+    const requests = await RequestCare.find(); //here RequestCare is the schema name
+    res.status(200).json(requests);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
