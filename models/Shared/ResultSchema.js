57481e763338e2e1c1c4f3142d773f27a6515bca
Aljoboyer <tanveeazmain@gmail.com>
2022-02-27 11:43:38 +0600

    Resutl Publish Done

--- models/Shared/ResultSchema.js
+++ models/Shared/ResultSchema.js
@@ -25,6 +25,163 @@ const ResultSchema = mongoose.Schema({
         type: String,
         required: false
     },
+    math:{
+        type: String,
+        required: false
+    },
+    bangla:{
+        type: String,
+        required: false
+    },
+    english:{
+        type: String,
+        required: false
+    },
+    science:{
+        type: String,
+        required: false
+    },
+    draw:{
+        type: String,
+        required: false
+    },
+
+    bangla1mid:{
+        type: String,
+        required: false
+    },
+    bangla2mid:{
+        type: String,
+        required: false
+    },
+    english1mid:{
+        type: String,
+        required: false
+    },
+    english2mid:{
+        type: String,
+        required: false
+    },
+    religionmid:{
+        type: String,
+        required: false
+    },
+    gsmid:{
+        type: String,
+        required: false
+    },
+    mathmid:{
+        type: String,
+        required: false
+    },
+    banglamid:{
+        type: String,
+        required: false
+    },
+    englishmid:{
+        type: String,
+        required: false
+    },
+    sciencemid:{
+        type: String,
+        required: false
+    },
+    drawmid:{
+        type: String,
+        required: false
+    },
+
+    bangla1final:{
+        type: String,
+        required: false
+    },
+    bangla2final:{
+        type: String,
+        required: false
+    },
+    english1final:{
+        type: String,
+        required: false
+    },
+    english2final:{
+        type: String,
+        required: false
+    },
+    religionfinal:{
+        type: String,
+        required: false
+    },
+    gsfinal:{
+        type: String,
+        required: false
+    },
+    mathfinal:{
+        type: String,
+        required: false
+    },
+    banglafinal:{
+        type: String,
+        required: false
+    },
+    englishfinal:{
+        type: String,
+        required: false
+    },
+    sciencefinal:{
+        type: String,
+        required: false
+    },
+    drawfinal:{
+        type: String,
+        required: false
+    },
+
+    bangla1total:{
+        type: String,
+        required: false
+    },
+    bangla2total:{
+        type: String,
+        required: false
+    },
+    english1total:{
+        type: String,
+        required: false
+    },
+    english2total:{
+        type: String,
+        required: false
+    },
+    religiontotal:{
+        type: String,
+        required: false
+    },
+    gstotal:{
+        type: String,
+        required: false
+    },
+    mathtotal:{
+        type: String,
+        required: false
+    },
+    banglatotal:{
+        type: String,
+        required: false
+    },
+    englishtotal:{
+        type: String,
+        required: false
+    },
+    sciencetotal:{
+        type: String,
+        required: false
+    },
+    drawtotal:{
+        type: String,
+        required: false
+    },
+
+
     firsttermCgpa:{
         type: String,
         required: false
@@ -33,11 +190,12 @@ const ResultSchema = mongoose.Schema({
         type: String,
         required: false
     },
-    mathCgpa:{
+    thirdtermCgpa:{
         type: String,
         required: false
     },
-    thirdtermCgpa:{
+
+    mathCgpa:{
         type: String,
         required: false
     },
@@ -65,10 +223,6 @@ const ResultSchema = mongoose.Schema({
         type: String,
         required: false
     },
-    Failcount1:{
-        type: String,
-        required: false
-    },
     banglaCgpa:{
         type: String,
         required: false
@@ -81,18 +235,24 @@ const ResultSchema = mongoose.Schema({
         type: String,
         required: false
     },
-    bangla:{
+    drawCgpa:{
+    type: String,
+    required: false
+    },
+
+    Failcount:{
         type: String,
         required: false
     },
-    english:{
+    Failcount1:{
         type: String,
         required: false
     },
-    science:{
+    Failcount3:{
         type: String,
         required: false
     },
+
     name:{
         type: String,
         required: false
