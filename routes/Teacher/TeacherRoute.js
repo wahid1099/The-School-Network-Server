3c82a84199b943e919238d1a6702375c50a9b3f7
Saad100912 <arsaad.2016@gmail.com>
2022-02-27 13:44:25 +0600

    added teachers publish notice api and get teacher information api

---
+++ routes/Teacher/TeacherRoute.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const RequestCare = require("../../models/Student/requestCare");
+const ResultSchema = require("../../models/Shared/ResultSchema");
+const ResultCollection = new mongoose.model("ResultCollection", ResultSchema);
+const NoticeSchema = require("../../models/Teacher/NoticeSchema");
+const StudentNoticeCollection = new mongoose.model(
+    "studentnoticecollection",
+    NoticeSchema
+);
+const userSchema = require("../../models/Shared/UserSchema");
+const userCollection = new mongoose.model("usercollection", userSchema);
+
+//geting all student extra care
+router.get("/requestCare", async (req, res) => {
+    const requests = await RequestCare.find(); //here RequestCare is the schema name
+    res.status(200).json(requests);
+});
+router.post("/PublishResult", async (req, res) => {
+    const result = new ResultCollection(req.body);
+    try {
+        await result.save();
+        res.send({ success: "success" });
+    } catch (er) {
+        console.log(er);
+    }
+});
+
+// Publish notice from teachers for students
+router.post("/PublishNotice", async (req, res) => {
+    const notice = new StudentNoticeCollection(req.body);
+    try {
+        await notice.save();
+        res.send({ success: "success" });
+    } catch (er) {
+        console.log(er);
+    }
+});
+
+// Get teachers information
+router.get("/TeacherProfile", async (req, res) => {
+    const teacherEmail = req.query.email;
+    const response = await userCollection.findOne({ email: teacherEmail });
+    res.send(response);
+});
+
+module.exports = router;
