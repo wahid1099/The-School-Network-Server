6a8e47f3c3ed7dd7d8479546aacfd83cd8a67033
KaziMdMehediHasan <=>
2022-03-02 00:23:21 +0600

    fitered out student

--- routes/Student/student.js
+++ routes/Student/student.js
@@ -5,6 +5,8 @@ const RequestCare = require("../../models/Student/requestCare");
 const { db } = require("../../models/Student/studentModels");
 const ResultSchema = require("../../models/Shared/ResultSchema");
 const ResultCollection = new mongoose.model("ResultCollection", ResultSchema);
+const UserSchema = require("../../models/Shared/UserSchema");
+const UserCollection = new mongoose.model("UserCollection", UserSchema);
 
 //Student notes Submit
 router.post("/notesSubmit", async (req, res) => {
@@ -47,6 +49,32 @@ router.get("/results", async (req, res) => {
   try {
     const results = await ResultCollection.find({});
     res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// fetch specific student from user collection with the help of email
+
+router.get("/filteredStudent/:email", async (req, res) => {
+  const email = req.params.email;
+  console.log(email);
+  try {
+    const filteredResult = await UserCollection.find({ email: email });
+    res.status(200).json(filteredResult);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get result according to student information
+
+router.get("/filteredResult", async (req, res) => {
+  const query = req.query;
+  console.log(query);
+  try {
+    const filteredResult = await ResultCollection.find(query);
+    res.status(200).json(filteredResult);
   } catch (err) {
     res.status(500).json(err);
   }
