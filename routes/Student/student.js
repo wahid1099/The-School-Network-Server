6348e17b010bbe0740b746ae7f1c4a6da22dc129
KaziMdMehediHasan <=>
2022-03-03 22:39:45 +0600

    transcript route bug fixed

--- routes/Student/student.js
+++ routes/Student/student.js
@@ -79,16 +79,46 @@ router.get("/filteredStudent", async (req, res) => {
 // get result according to student information
 
 router.get("/filteredResult", async (req, res) => {
-  const query = req.query;
-  const student = await UserCollection.findOne(query);
+  const query = req.query.email;
+  const student = await UserCollection.findOne({ email: query });
   // res.send(student);
   console.log(query);
   try {
-    const filteredResult = await ResultCollection.find({ name: student.name });
-    res.status(200).json({ filteredResult, student });
+    const filteredResult = await ResultCollection.find({
+      name: student.name,
+      roll: student.roll,
+      class: student.class,
+    });
+    // console.log({ filteredResult, student });
+    res.status(200).json(filteredResult);
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+// Get single student information
+router.get("/studentProfile", async (req, res) => {
+  const studentEmail = req.query.email;
+  const response = await UserCollection.findOne({ email: studentEmail });
+  res.send(response);
+});
+
+// Update single student profile picture
+router.put("/updateStudentPP", async (req, res) => {
+  const email = req.query.email;
+  const front = req.files.userImage.data;
+  // console.log(email);
+
+  const encodedpic1 = front.toString("base64");
+  const img = Buffer.from(encodedpic1, "base64");
+
+  const query = { email: email };
+  const update = await UserCollection.findOneAndUpdate(
+    query,
+    { $set: { img: img } },
+    { upsert: true }
+  );
+  res.send(update);
 });
 
 module.exports = router;
