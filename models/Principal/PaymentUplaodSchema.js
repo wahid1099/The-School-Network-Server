ee42e86918d34ce1bc069bfb10c5a8314d8da152
Aljoboyer <tanveeazmain@gmail.com>
2022-03-04 15:59:45 +0600

    done

--- models/Principal/PaymentUplaodSchema.js
+++ models/Principal/PaymentUplaodSchema.js
@@ -17,23 +17,7 @@ const MonthlyPayment = mongoose.Schema({
         type: String,
         required: false
     },
-    bloodGroup:  {
-        type: String,
-        required: false
-    },
-    age:  {
-        type: String,
-        required: false
-    },
     email:  {
-        type: String,
-        required: false
-    },
-    password:  {
-        type: String,
-        required: false
-    },
-    birthCertificateNo:  {
         type: String,
         required: false
     },
@@ -41,47 +25,7 @@ const MonthlyPayment = mongoose.Schema({
         type: String,
         required: false
     },
-    birthDate:  {
-        type: String,
-        required: false
-    },
-    fatherName:  {
-        type: String,
-        required: false
-    },
-    motherName:  {
-        type: String,
-        required: false
-    },
-    address:  {
-        type: String,
-        required: false
-    },
     role:  {
-        type: String,
-        required: false
-    },
-    teachername:  {
-        type: String,
-        required: false
-    },
-    teacherclass:  {
-        type: String,
-        required: false
-    },
-    teachersection:  {
-        type: String,
-        required: false
-    },
-    teachernid:  {
-        type: String,
-        required: false
-    },
-    img:{
-        type: mongoose.Mixed,
-        required: false
-    },
-    teacherphone:{
         type: String,
         required: false
     },
