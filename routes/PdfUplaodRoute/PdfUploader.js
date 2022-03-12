7179e604c09582e29ddbca99cd5296e6501b023d
Abdul Wahid <wahdiahmed890@gmail.com>
2022-03-12 21:06:34 +0600

    adding routes for pdf fetching data

--- routes/PdfUplaodRoute/PdfUploader.js
+++ routes/PdfUplaodRoute/PdfUploader.js
@@ -33,4 +33,23 @@ router.post("/pdfUpload", async (req, res) => {
   });
 });
 
+router.get("/GetAllPdfs", async (req, res) => {
+  try {
+    // no need for database name, only the schema name is enough to fetch data
+    const studentpDf = await PdfModel.find(); //here RequestCare is the schema name
+    res.status(200).json(studentpDf);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// fetch the Pdf  data of a student
+router.get("/studentPdfID", async (req, res) => {
+  const StudentId = req.query.StudentId;
+  console.log(StudentId);
+  const response = await PdfModel.findOne({ StudentId: StudentId });
+  console.log(response);
+  res.send(response);
+});
+
 module.exports = router;
