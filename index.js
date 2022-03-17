5aa8652eb7aa52ce71a674d3077f4f5dee7e5a40
Aljoboyer <tanveeazmain@gmail.com>
2022-03-17 19:41:02 +0600

    d

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use(express.urlencoded({ extended: true }));
 
 // -----------Shared Roudets start---------//
