bc02dfc90b79c384b3c8d5c1bb7e7f4c11b00464
Aljoboyer <tanveeazmain@gmail.com>
2022-03-17 18:30:21 +0600

    done

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ const pdfuploads = require("./routes/PdfUplaodRoute/PdfUploader");
 connectDB();
 
 //middleware
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
