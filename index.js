0d1339c6a1649cb3215414e533322bd4ae779c9e
Aljoboyer <tanveeazmain@gmail.com>
2022-03-18 22:07:09 +0600

    done

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(fileUpload());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.urlencoded({ extended: true })); 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next()
+})
 // ---Database connection
 connectDB();
 
