81ac9ec17b0b4305af151e7edba5c99cdcfc60b2
Aljoboyer <tanveeazmain@gmail.com>
2022-03-19 19:16:57 +0600

    d

--- config/AllowedOrigins.js
+++ config/AllowedOrigins.js
@@ -1,7 +1,7 @@
 
 const allowedOrigins = [
   'https://blooming-citadel-14218.herokuapp.com',
-  'https://blooming-citadel-14218.herokuapp.com/',
+  'http://localhost:5000/',
   'https://the-school-network.web.app',
   'https://the-school-network.web.app/',
   'http://127.0.0.1:5000',
