6ae58b1c956f78bdba15b084536311f9e283b028
Aljoboyer <tanveeazmain@gmail.com>
2022-03-25 09:56:55 +0600

    d

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
+const {ServerApiVersion } = require('mongodb');
 
 const connectDB = async () => {
   const mongouri = `mongodb+srv://${process.env.USERDB}:${process.env.USERPASS}@cluster0.vsy2x.mongodb.net/TheSchoolNetwork?retryWrites=true&w=majority`;
   try {
-       mongoose.connect(
+       mongoose.connect( 
       mongouri,
       {
         useUnifiedTopology: true,
         useNewUrlParser: true,
+        serverApi: ServerApiVersion.v1
       },
       console.log("connected to database")
     );
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+require('dotenv').config();
 const fileUpload = require("express-fileupload");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
